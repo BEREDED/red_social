ed12f4cf28bfc1de185bca34b86d6c165d586d4e
Zention <contact.zentyon@gmail.com>
2025-05-13 18:41:08 -0600

    chat minor fix

--- src/app/chats/chats-routing.module.ts
+++ src/app/chats/chats-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainChatComponent } from './main-chat/main-chat.component';
-import { SidePanelComponent } from './side-panel/side-panel.component';
-
-import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [{
   path: '',
--- src/app/chats/chats.module.ts
+++ src/app/chats/chats.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular'; // Importar IonicModule
 
 import { ChatsRoutingModule } from './chats-routing.module';
-
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
+import { ListaComponent } from './lista/lista.component';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -13,11 +14,14 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     MainChatComponent,
     SidePanelComponent,
+    ListaComponent,
   ],
   imports: [
     CommonModule,
     ChatsRoutingModule,
     SharedModule,
+    IonicModule,
+
   ]
 })
 export class ChatsModule { }
