f5d6a9d1ae7b3afaadccc2201be17845b82c634b
Zention <contact.zentyon@gmail.com>
2025-06-08 16:26:53 -0600

    api stuff
    carpeta functions con toda la wea de la api

---
+++ functions/src/keys.ts
@@ -0,0 +1,13 @@
+export default {
+    database: {
+        user: 'Zeniith',
+        password: 'Nethub01',
+        server: 'nethub.database.windows.net',
+        database: 'red_social_v1',
+        options: {
+            encrypt: false,
+            trustServerCertificate: false
+
+        }
+    }
+}
