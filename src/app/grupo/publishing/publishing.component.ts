ece92b9b1f1a8a75072b00ec6e30cd36b89ed474
BEREDED <alvarezdavid420@gmail.com>
2025-06-19 01:56:03 -0600

    ver si es eso lo que falta en el despliegue

--- src/app/grupo/publishing/publishing.component.ts
+++ src/app/grupo/publishing/publishing.component.ts
@@ -13,6 +13,7 @@ export class PublishingComponent {
   @Input() is_creador:boolean=false;
   @Input() Titulo_grupo: string = '';
   postContent=''
+  Fecha_cad=''
   isTarea=false
   postcreate:Post={
     Titulo_foro:'',
@@ -40,24 +41,30 @@ export class PublishingComponent {
   onPost(): void {
     if (this.canPost()) {
       console.log("prueba de si selecciono la casilla",this.isTarea)
-      const fecha=new Date()
-      const fechaFormateada = fecha.toISOString().slice(0, 16).replace('T', ' ');
-      this.postcreate.Contenido=this.postContent
-      this.postcreate.correo_Usuario=String(localStorage.getItem('correoGlobal'))
-      this.postcreate.Titulo_foro=this.Titulo_grupo
-      this.postcreate.Fecha_Publicacion=new Date(fechaFormateada)
-      this.usuariosService.postCrearPostgrp(this.postcreate).subscribe({
-        next: (response) =>{
-          console.log(response.Mensaje)
-          this.nuevoPost.emit();
-        },
-        error: (error) =>{
-          console.error(error)
-        }
-      })
-      console.log("esto es el post", this.postcreate)
-      console.log('Publicando:', this.postContent);
-      this.postContent=''
+      console.log("prueba de fecha de caducacion", this.Fecha_cad)
+      if(this.isTarea===false){
+        const fecha=new Date()
+        const fechaFormateada = fecha.toISOString().slice(0, 16).replace('T', ' ');
+        this.postcreate.Contenido=this.postContent
+        this.postcreate.correo_Usuario=String(localStorage.getItem('correoGlobal'))
+        this.postcreate.Titulo_foro=this.Titulo_grupo
+        this.postcreate.Fecha_Publicacion=new Date(fechaFormateada)
+        this.usuariosService.postCrearPostgrp(this.postcreate).subscribe({
+          next: (response) =>{
+            console.log(response.Mensaje)
+            this.nuevoPost.emit();
+          },
+          error: (error) =>{
+            console.error(error)
+          }
+        })
+        console.log("esto es el post", this.postcreate)
+        console.log('Publicando:', this.postContent);
+        this.postContent=''
+      }
+      else{
+        console.log("esto es una publicacion de tarea")
+      }
     }
   }
 }
